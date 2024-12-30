1222981285f29e99770417f08a45f614d4b2dc68
faridaelhusseiny278 <faridaelhussieny@gmail.com>
2024-12-30 19:31:51 +0200

    added privilege control panel and organization type control panel

--- Database/User_Address.sql
+++ Database/User_Address.sql
@@ -2,6 +2,6 @@ CREATE TABLE User_Address (
     UserID INT,
     AddressID INT,
     PRIMARY KEY (UserID, AddressID),
-    FOREIGN KEY (UserID) REFERENCES User(UserID),
-    FOREIGN KEY (AddressID) REFERENCES Location(AddressID)
+    FOREIGN KEY (UserID) REFERENCES User(UserID) ON DELETE CASCADE,
+    FOREIGN KEY (AddressID) REFERENCES Location(AddressID) ON DELETE CASCADE
 );
